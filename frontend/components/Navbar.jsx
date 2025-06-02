bfccb9e83a3cd27c4185be51b780c0a0b89b863e
Kshitij Gupta <kshitijgupta2162@gmail.com>
2025-06-02 17:36:08 +0530

    updated

--- frontend/components/Navbar.jsx
+++ frontend/components/Navbar.jsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                   About
                 </a>
                 <a
-                  href="/programs"
+                  href="#"
                   className="text-gray-700 hover:text-blue-600 px-3 py-2 rounded-md text-sm font-medium transition-colors"
                 >
                   Blogs
